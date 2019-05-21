170dc4b6dee21f6f1fd9e74bc099f598ee9785c2
moajo <mimirosiasd@gmail.com>
2019-05-21 14:32:01 +0900

    implement post page

--- server_rails/app/controllers/pages_controller.rb
+++ server_rails/app/controllers/pages_controller.rb
@@ -1,3 +1,15 @@
+def nilOrEmpty(str)
+  str == nil || str == ""
+end
+
+def to_s_or_null(str)
+  if str == nil
+    return nil
+  end
+  i = str.to_i
+  str == i.to_s ? i : nil
+end
+
 class PagesController < ApplicationController
   before_action :set_page, only: [:show, :update, :destroy]
 
@@ -8,20 +20,54 @@ class PagesController < ApplicationController
     render json: @pages
   end
 
-  # GET /pages/1
-  def show
-    render json: @page
-  end
+  # # GET /pages/1
+  # def show
+  #   render json: @page
+  # end
 
   # POST /pages
   def post
-    @page = Page.new(page_params)
+    json_request = JSON.parse(request.body.read)
+    name = json_request["name"]
+    text = json_request["text"]
+    storyIdStr = params["storyId"]
+    parentIdStr = params["parentId"]
 
-    if @page.save
-      render json: @page, status: :created, location: @page
-    else
-      render json: @page.errors, status: :unprocessable_entity
+    if nilOrEmpty(name)
+      puts "name is nil"
+      head 400
+      return
     end
+
+    if nilOrEmpty(text)
+      puts "text is nil"
+      head 400
+      return
+    end
+
+    storyId = to_s_or_null(storyIdStr)
+    if storyId == nil
+      puts "storyId is nil"
+      head 400
+      return
+    end
+
+    parentId = to_s_or_null(parentIdStr)
+    if parentId == nil
+      puts "parentId is nil"
+      head 400
+      return
+    end
+
+    parent = Page.find_by(id: parentId, story_id: storyId)
+    if parent == nil
+      puts "parent is not exists"
+      head 404
+      return
+    end
+
+    createdPage = Page.create(name: name, text: text, story_id: storyId, parent_id: parent.id)
+    render json: { "id": createdPage.id }
   end
 
   # PATCH/PUT /pages/1
